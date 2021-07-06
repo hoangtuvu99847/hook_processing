04df4b0d0206aa6b1c054980bfe3a5e679e318d4
vunv <vunv@mobio.vn>
2021-07-06 11:16:50 +0700

    Remove vuejs

---
+++ tools/collection_tool.py
@@ -0,0 +1,79 @@
+import psutil
+import sys
+
+
+class CollectionTool:
+    def __init__(self):
+        self.THRESHOLD = 100 * 1024 * 1024  # 100MB
+
+    @staticmethod
+    def convert_byte_to_gb(value):
+        return round(value / (1024.0 ** 3), 1)
+
+    def ram(self):
+        memory = psutil.virtual_memory()
+
+        ram_obj = {
+            'available': memory.available,
+            'total': self.convert_byte_to_gb(psutil.virtual_memory().total),
+            'percent': memory.percent
+        }
+        return ram_obj
+
+    def cpu(self):
+        cpu_per = psutil.cpu_percent(interval=1, percpu=True)
+        list_cpu = []
+        for i, core_per in enumerate(cpu_per):
+            cpu_dict = {}
+            cpu_dict['cpu_name'] = 'cpu_{count}'.format(count=i+1)
+            cpu_dict['percent'] = core_per
+            list_cpu.append(cpu_dict)
+        return list_cpu
+
+    def disk(self):
+        pass
+
+    def network(self):
+        pass
+
+    def sensor(self):
+        list_cpu_temp = []
+        if not hasattr(psutil, "sensors_temperatures"):
+            sys.exit("platform not supported")
+        temps = psutil.sensors_temperatures()
+        if not temps:
+            sys.exit("can't read any temperature")
+        if 'coretemp' in temps:
+            for entries in temps['coretemp']:
+                temp = {
+                    'label': entries.label,
+                    'current': entries.current,
+                    'high': entries.high,
+                    'critical': entries.critical,
+                }
+                list_cpu_temp.append(temp)
+        return list_cpu_temp
+
+    def _ex(self):
+        pass
+
+    def run(self):
+        try:
+
+            cpu_status = self.cpu()
+            ram_status = self.ram()
+            sensor_status = self.sensor()
+            disk_status = self.disk()
+            network_status = self.network()
+            data = {
+                'cpu': cpu_status,
+                'ram': ram_status,
+                'sensor': sensor_status,
+                'disk': disk_status,
+                'network': network_status
+            }
+            return data
+
+        except Exception as ex:
+            print(ex)
+            pass
---
+++ tools/main.py
@@ -0,0 +1,67 @@
+# RUN HERE
+from collection_tool import CollectionTool
+import socket
+import paho.mqtt.client as mqtt
+import json
+from datetime import datetime
+
+LOCALHOST = '172.16.0.121'
+BROKER_PORT = 1883
+
+
+class Send:
+    def __init__(self):
+        self.client = mqtt.Client()
+        self.tool = CollectionTool()
+        self.hostname = socket.gethostname()
+        self.ip = socket.gethostbyname(self.hostname)
+        self.prefix_topic = 'server/'
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        self.client.subscribe(self.ip)
+
+    def create_time_now(self):
+        return datetime.now()
+
+    def on_message(client, userdata, msg):
+        print('==> ', msg.topic+" "+str(msg.payload))
+
+    def public_to_broker(self):
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.connect(LOCALHOST, BROKER_PORT, 60)
+        try:
+            while True:
+                result = CollectionTool().run()
+                result.update({
+                    'client': self.hostname,
+                    'ip': self.ip
+                })
+                json_data = json.dumps(result).encode('utf-8')
+                print(json_data)
+                self.client.publish(self.prefix_topic + self.ip, json_data)
+        except Exception as ex:
+            print("Send -> public_to_broker :: ", ex)
+
+    def save_information(self):
+        import sqlite3
+        try:
+            connection = sqlite3.connect('../hookprocessing.db')
+            cursor = connection.cursor()
+            sql = """
+            INSERT INTO server (hostname, ip_address, created_time, updated_time) 
+            VALUES (?, ?, ?, ?)
+            """
+            values = (self.hostname, self.ip,
+                      self.create_time_now(), self.create_time_now())
+            cursor.execute(sql, values)
+            connection.commit()
+            self.public_to_broker()
+        except Exception as ex:
+            print("Send -> save_information :: ", ex)
+            return
+
+
+if __name__ == '__main__':
+    Send().public_to_broker()
